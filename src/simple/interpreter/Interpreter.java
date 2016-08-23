09ef9e0055afa08060bba3c0c17f573d68c40b1d
jcarlos <carlosmaster10@gmail.com>
2016-08-23 13:19:58 -0300

    minor fixes

--- src/simple/interpreter/Interpreter.java
+++ src/simple/interpreter/Interpreter.java
@@ -5,7 +5,7 @@ import simple.interpreter.opcodes.OpcodeFactory;
 import simple.interpreter.util.StringToInteger;
 
 public class Interpreter {
-    
+
     private static final int OPCODE_DONT_EXISTS = -2;
     private static final int INVALID_INSTRUCTION = -1;
     static int PC = 0;
@@ -19,7 +19,7 @@ public class Interpreter {
     public static void interpret(String[] memory, int starting_address) {
 
         PC = starting_address;
-        System.out.println("INIT AC = "+AC);
+        System.out.println("INIT AC = " + AC);
         while (run_bit && PC < memory.length) {
             instr = memory[PC];
             PC = PC + 1;
@@ -30,18 +30,17 @@ public class Interpreter {
                 System.out.println("HALT called");
                 break;
             }
-            
+
             data_loc = find_data(instr, instr_type);
             if (data_loc == OPCODE_DONT_EXISTS) {
                 continue;
-            }
-            else if (data_loc >= 0) {
+            } else if (data_loc >= 0) {
                 data = memory[data_loc];
             }
             execute(instr_type, data);
         }
-        
-        System.out.println("AC VALUE = "+AC);
+
+        System.out.println("AC VALUE = " + AC);
     }
 
     private static int get_instr_type(String _inst) {
@@ -49,7 +48,7 @@ public class Interpreter {
     }
 
     private static int find_data(String _inst, int _type) {
-        
+
         Opcode opcode = OpcodeFactory.getOpcode(_type);
         if (opcode == null) {
             return OPCODE_DONT_EXISTS;
@@ -58,9 +57,9 @@ public class Interpreter {
                 return INVALID_INSTRUCTION;
             }
         }
-        
+
         ++PC;
-        return PC - 1;
+        return (PC - 1);
     }
 
     private static void execute(int _type, String _data) {
@@ -68,19 +67,19 @@ public class Interpreter {
         if (opcode == null) {
             return;
         }
-        
+
         int number;
-        
+
         StringToInteger sInteger = StringToInteger.getInstance();
         sInteger.setStringValue(_data);
-        
+
         try {
             if (sInteger.isInteger() == true) {
                 number = opcode.exec(AC, sInteger.getIntegerValue());
             } else {
                 number = opcode.exec(AC, 0);
             }
-            
+
         } catch (Exception e) {
             return;
         }
@@ -89,19 +88,9 @@ public class Interpreter {
 
     public static void main(String[] args) {
         /*
-         * Commands:
-         * ADD
-         * SUB
-         * DIV
-         * MUL
-         * DOUBLE
-         * ABS
-         * EXP
-         * INCREMENT
-         * DECREMENT
-         * HALT
+         * Commands: ADD SUB DIV MUL DOUBLE ABS EXP INCREMENT DECREMENT HALT
          */
-        
+
         Interpreter.interpret(args, 0);
     }
 }
